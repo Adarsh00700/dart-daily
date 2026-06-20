8746c0f617b83147e5cedcc40f69bb11aef4bc50
unknown <adarshpvarun@gmail.com>
2026-06-20 19:58:29 +0530

    Updated code

--- day1.dart
+++ day1.dart
@@ -1,7 +1,10 @@
 void main() {
   calculateSalary(25000, 10);
+
+  calculateAverage(80, 75, 90);
 }
 
+//bonus
 double calculateSalary(double Salary, double bounsPercentage) {
   double bonus = Salary * bounsPercentage / 100;
   print("bouns=$bonus");
@@ -11,4 +14,13 @@ double calculateSalary(double Salary, double bounsPercentage) {
 
   print("Total salary=$totalSalary");
   return totalSalary;
+}
+
+//mark
+
+double calculateAverage(int mark1, int mark2, int mark3) {
+  double average = (mark1 + mark2 + mark3) / 3;
+
+  print("Average Marks = $average");
+  return average;
 }
