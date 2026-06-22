e0dae8f93605ae7f9956c441d4532bc28881c3a9
Adarsh00700 <adarshpvarun@gmail.com>
2026-06-22 15:57:23 +0530

    Updated logical questions

--- day2.dart
+++ day2.dart
@@ -2,6 +2,12 @@ void main() {
   checkEligibility(20);
 
   checkEligibility(14);
+
+  String username = "Admin";
+  String password = "1234";
+
+  String result = LoginCheck(username, password);
+  print(result);
 }
 
 void checkEligibility(int age) {
@@ -9,5 +15,13 @@ void checkEligibility(int age) {
     print("$age Eligible to vote");
   } else {
     print("$age minor not eligible");
+  }
+}
+
+String LoginCheck(String username, String password) {
+  if (username == "Admin" && password == "1234") {
+    return ("Login Success");
+  } else {
+    return ("invalid");
   }
 }
